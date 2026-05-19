7ff32092872c2c17ceb9f9b2bb86ac9ff6d01f93
Caio de Souza <caiodesouza.cds@gmail.com>
2026-05-19 15:02:50 -0300

    start: commit inicial

---
+++ src/model/Categoria.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+namespace model;
+
+class Categoria{
+    public function __construct(
+        private int $id,
+        private string $nome
+    ){}
+
+    public function getId(): int{
+        return $this->id;
+    }
+
+    public function setId(int $id): void{
+        $this->id = $id;
+    }
+
+    public function getNome(): string{
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void{
+        $this->nome = $nome;
+    }
+
+}
